8f69a821ac9170ab515cf98c47c0e65e8eaf3557
Juan Mireles <119257668+Kal0yanX@users.noreply.github.com>
2023-08-07 09:19:41 -0400

    sessions comments working

--- frontend/src/places/NewCommentForm.js
+++ frontend/src/places/NewCommentForm.js
@@ -8,23 +8,9 @@ function NewCommentForm({ place, onSubmit }) {
     const [comment, setComment] = useState({
         content: '',
         stars: 3,
-        rant: false,
-        authorId: ''
+        rant: false
     })
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const response = await fetch(`http://localhost:5000/users`)
-            const users = await response.json()
-            setComment({ ...comment, authorId: users[0]?.userId})
-            setAuthors(users)
-        }
-        fetchData()
-    }, [])
-
-    let authorOptions = authors.map(author => {
-        return <option key={author.userId} value={author.userId}>{author.firstName} {author.lastName}</option>
-    })
 
     function handleSubmit(e) {
         e.preventDefault()
@@ -53,12 +39,6 @@ function NewCommentForm({ place, onSubmit }) {
                 </div>
             </div>
             <div className="row">
-                <div className="form-group col-sm-4">
-                    <label htmlFor="state">Author</label>
-                    <select className="form-control" value={comment.authorId} onChange={e => setComment({ ...comment, authorId: e.target.value })}>
-                        {authorOptions}
-                    </select>
-                </div>
                 <div className="form-group col-sm-4">
                     <label htmlFor="stars">Star Rating</label>
                     <input
